471eeee1d04ad31693d2afff95a6773d735e7536
堀江雄一 <>
2025-08-23 12:32:43 +0900

    fix: use split env vars for Firebase Admin SDK

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,6 +8,7 @@ const firebaseApp =
         credential: cert({
           projectId: process.env.FIREBASE_PROJECT_ID,
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          // .env.localでは \n の形、ここで本物の改行に変換
           privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
         }),
       })
