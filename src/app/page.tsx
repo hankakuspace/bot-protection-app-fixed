36fb9c927b03f153da0a65240114b178e5a8949d
堀江雄一 <>
2025-09-19 19:00:25 +0900

    fix: TOPページをダッシュボードへリダイレクトするよう修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,6 @@
 // src/app/page.tsx
-"use client";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>ナビゲーションテスト</h1>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-            <ui-nav-menu>
-              <a href="/admin/dashboard">ダッシュボード</a>
-              <a href="/admin/admin-ip">管理者IP</a>
-              <a href="/admin/block-ip">ブロックIP</a>
-              <a href="/admin/logs">アクセスログ</a>
-            </ui-nav-menu>
-          `,
-        }}
-      />
-    </div>
-  );
+  redirect("/admin/dashboard");
 }
