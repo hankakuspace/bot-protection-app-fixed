05da9bc42e2c4febe05ef04bb2a1c3393bb47d6a
堀江雄一 <>
2025-10-03 11:50:53 +0900

    chore: add JSX type definition for ui-nav-menu custom element

---
+++ src/types/custom-elements.d.ts
@@ -0,0 +1,13 @@
+// src/types/custom-elements.d.ts
+import React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "ui-nav-menu": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
