9cab85765f67c8eb275471a0106c98d0590f582e
堀江雄一 <>
2025-10-04 16:29:05 +0900

    fix: ui-nav-menu の型定義を next-env.d.ts に統合して Vercel ビルドエラーを解消

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,19 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+
+// 👇 ここから追記
+import React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "ui-nav-menu": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
+
+export {};
