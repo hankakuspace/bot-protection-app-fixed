e5f3629ec3f265d00cfda0af1594afb3d119565c
堀江雄一 <horie@hankaku-space.jp>
2026-03-19 11:33:17 +0900

    TailwindとPostCSSのビルド設定を修正

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+// postcss.config.js
 module.exports = {
-  content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    "@tailwindcss/postcss": {},
   },
-  plugins: [],
 };
